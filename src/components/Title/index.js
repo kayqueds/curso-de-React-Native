20330aa51c0422c139f2f09dec64064873486478
Kayque Estevão <kayque2911@gmail.com>
2025-06-22 17:03:10 -0300

    Primeiro commit

---
+++ src/components/Title/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default function Title() {
+  return (
+    <View>
+      <Text>
+        OneBitHealth...
+      </Text>
+    </View>
+  );
+}
+
+
+
+
+
